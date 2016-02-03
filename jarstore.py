e63e13425642f2841d57548c9195886423770424
Andrew Reid <andrew.k.reid@gmail.com>
2016-02-03 16:50:26 +1100

    WIP

--- jarstore.py
+++ jarstore.py
@@ -32,22 +32,61 @@ class JarStore:
                 user_id TEXT NOT NULL,
                 user_name TEXT NOT NULL,
                 when_swore TEXT NOT NULL,
-                swear_word TEXT NOT NULL
+                swear_word TEXT NOT NULL,
+                cents INT NOT NULL
             )''')
+            self._conn.execute('''
+            CREATE TABLE IF NOT EXISTS payments
+            (
+                user_id TEXT NOT NULL,
+                user_name TEXT NOT NULL,
+                when_paid TEXT NOT NULL,
+                cents INT NOT NULL
+            )
+           ''')
 
-    def add_swear(self, user_id, user_name, swear_word):
+    def add_swear(self, user_id, user_name, swear_word, fine):
         when_swore = datetime.datetime.now().isoformat()
         query = '''
-            INSERT INTO swears (user_id, user_name, when_swore, swear_word) VALUES (?, ?, ?, ?)
+            INSERT INTO swears (user_id, user_name, when_swore, swear_word, cents) VALUES (?, ?, ?, ?, ?)
         '''
-        args = [user_id, user_name, when_swore, swear_word]
+        args = [user_id, user_name, when_swore, swear_word, fine]
         with self._conn:
             self._conn.execute(query, args)
 
     def get_swear_total(self):
         with self._conn:
-            sql = self._conn.execute("SELECT COUNT(*) as num_rows from swears")
+            sql = self._conn.execute("SELECT SUM(cents) as num_rows from swears")
             row = sql.fetchone()
-            return row[0]
+            if row:
+                return row[0]
+            else:
+                return 0
+
+    def get_money_owed(self, user_id):
+        with self._conn:
+            # total the fines
+            sql = self._conn.execute("SELECT SUM(cents) FROM swears WHERE user_id = ?", [user_id])
+            row = sql.fetchone()
+            if row:
+                fines = int(row[0])
+            else:
+                fines = 0
+        return fines
+
+    def get_money_paid(self, user_id):
+        with self._conn:
+            # total the payments
+            sql = self._conn.execute("SELECT SUM(cents) FROM payments WHERE user_id = ?", [user_id])
+            row = sql.fetchone()
+            if row and row[0]:
+                payments = int(row[0])
+            else:
+                payments = 0
+        return payments
+
+
+
+
 
 
--- swearjar.py
+++ swearjar.py
@@ -3,17 +3,20 @@
 import time
 import json
 import logging
+import re
 from string import punctuation
 from jarstore import JarStore
 
 from slackclient import SlackClient
 
 CENTS_PER_SWEAR = 20
+BOT_NAME = "swearjar"
 
 # Globals
 bad_words = {}
 known_users = {}
 sent_msg_id = 1
+bot_user_id = 'U0L4KJ8MV'
 
 
 def tokenize_to_words(msg_text):
@@ -40,6 +43,7 @@ def get_user_info(user_id):
     global known_users
     global user_sc
     global user_token
+    global bot_user_id
 
     if user_id in known_users:
         logging.debug("Found %s in the cache" % user_id)
@@ -47,10 +51,11 @@ def get_user_info(user_id):
 
     logging.debug("Looking up user id [%s]" % user_id)
     user_object = json.loads(user_sc.api_call(method="users.info", user=user_id))
-    print user_object
-    print type(user_object)
     if "ok" in user_object and user_object["ok"] == True:
         known_users[user_id] = user_object
+        # set the ID for the bot user
+        if user_object["user"]["name"] == BOT_NAME:
+            bot_user_id = user_id
         return user_object
     else:
         return None
@@ -59,7 +64,63 @@ def get_user_info(user_id):
 def current_jar_total():
     global jar_store
     swears = jar_store.get_swear_total()
-    return "$%.2f" % (float(swears) * CENTS_PER_SWEAR / 100.0)
+    return "$%.2f" % (float(swears) / 100.0)
+
+
+def user_id_to_name(user_id):
+    user_object = get_user_info(user_id)
+    if user_object:
+        user_name = user_object["user"]["name"]
+    else:
+        user_name = user_id
+    return user_name
+
+
+def process_direct_message(msg):
+    """Handle people talking to the bot directly with '@swearjar command'"""
+    global bot_sc
+    global jar_store
+    global bot_user_id
+
+    # extract the command part
+    user_id = msg["user"]
+    bot_id_regexp = "<@%s>: (.*)$" % bot_user_id
+    logging.info("regexp is [%s]" % bot_id_regexp)
+    p = re.compile(bot_id_regexp)
+    m = p.match(msg["text"])
+    if m:
+        command = m.group(1)
+        logging.info("You said [%s] to swearjar" % command)
+
+        help_reply_text = ("Welcome to SwearJar, turning your @#$%! into $$$$\n"
+                           "\n"
+                           "Things you can ask/tell me:\n\n"
+                           "help - this text.\n"
+                           "total - print the swear jar total.\n"
+                           "leaders - print who is swearing the most.\n"
+                           "my swears - list all my swearing.\n"
+                           "<word> is not a swear word - remove <word> from list of naughty words.\n"
+                           "<word> is a  swear word - add <word> to the list of naughty words.\n"
+                           "what's the damage - list how much you owe.\n"
+                           "pay $nn.nn - record that you paid some money to the swear jar."
+                           )
+
+        if command.startswith("help"):
+            reply_text = help_reply_text
+        elif command.startswith("total"):
+            reply_text = "Swear Jar is currently owed %s" % current_jar_total()
+        elif "what's the damage" in command:
+            logging.debug("What's the damage for [%s]" % user_id)
+            fines = jar_store.get_money_owed(user_id) / 100.0
+            payments = jar_store.get_money_paid(user_id) / 100.0
+            reply_text = "%s has $%.2f in files, and has paid $%.2f, so they owe $%.2f" % (user_id_to_name(user_id),
+                                                                                           fines,
+                                                                                           payments,
+                                                                                           fines - payments)
+        else:
+            reply_text = "I don't understand that (try \"help\")"
+
+        bot_sc.rtm_send_message(channel=msg["channel"], message=reply_text)
 
 
 def process_message(msg):
@@ -69,24 +130,35 @@ def process_message(msg):
 
     if "type" in msg:
         if msg["type"] == "message" and "text" in msg:
-            swear_words = find_swearing(msg["text"])
-            if len(swear_words) > 0:
 
-                user_object = get_user_info(msg["user"])
-                if user_object:
-                    user_name = user_object["user"]["name"]
+            # Check for messages directly to the bot
+            if bot_user_id and "<@%s>" % bot_user_id in msg["text"]:
+                logging.info("Message to the bot [%s]" % msg["text"])
+                process_direct_message(msg)
+            else:
+                if bot_user_id and msg["user"] == bot_user_id:
+                    # Don't ding the bot for swearing.
+                    pass
                 else:
-                    user_name = msg["user"]
+                    # Check for swear words
+                    swear_words = find_swearing(msg["text"])
+                    if len(swear_words) > 0:
 
-                # Add to the swear jar
-                for swear_word in swear_words:
-                    jar_store.add_swear(msg["user"], user_name, swear_word)
-                # send a reply (TODO: send picture with web API)
-                bot_sc.rtm_send_message(channel=msg["channel"],
-                                        message="Oooo - %s said %s. Swear Jar is up to %s" % (user_name,
-                                                                                              " ".join(swear_words),
-                                                                                              current_jar_total()))
-                sent_msg_id += 1
+                        user_name = user_id_to_name(msg["user"])
+                        # Add to the swear jar
+                        for swear_word in swear_words:
+                            jar_store.add_swear(msg["user"], user_name, swear_word, CENTS_PER_SWEAR)
+                        # send a reply (TODO: send picture with web API)
+                        bot_sc.rtm_send_message(channel=msg["channel"],
+                                                message="Oooo - %s said %s. Swear Jar is up to %s" % (user_name,
+                                                                                                      " ".join(swear_words),
+                                                                                                      current_jar_total()))
+                        sent_msg_id += 1
+        elif msg["type"] == "presence_change" and "user" in msg:
+            """We get one of these after we start the bot saying the bot has joined the channel.
+               We use this to find out the bot's user ID"""
+            user_name = user_id_to_name(msg["user"])
+            logging.debug("user %s(%s)'s status changed to %s" % (msg["user"], user_name, msg["presence"]))
 
 
 if __name__ == '__main__':
